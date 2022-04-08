a21cd5646fafac3754c18859233da9af3aea531a
dikasyanov <dikanemo@gmail.com>
2022-04-09 01:19:34 +0200

    init commit

---
+++ src/main/java/com/switchkit/switchkit_test/SwitchkitTestApplication.java
@@ -0,0 +1,13 @@
+package com.switchkit.switchkit_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchkitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchkitTestApplication.class, args);
+	}
+
+}
